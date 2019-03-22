d0570b6679d5d76e9a331e9ed4d2d8a4f473bbed
nrc4867 <nrc4867@rit.edu>
2019-03-21 20:29:14 -0400

    Added the routes for the different AJAX calls, none of these classes have fleshed out implementation but stop the AJAX errors in browser

---
+++ src/main/java/com/webcheckers/ui/CheckersPlay/PostValidateRoute.java
@@ -0,0 +1,20 @@
+package com.webcheckers.ui.CheckersPlay;
+
+import com.google.gson.Gson;
+import com.webcheckers.util.Message;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class PostValidateRoute implements Route {
+    private final Gson gson;
+
+    public PostValidateRoute(Gson gson) {
+        this.gson = gson;
+    }
+
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        return gson.toJson(Message.error("No True implementation"));
+    }
+}

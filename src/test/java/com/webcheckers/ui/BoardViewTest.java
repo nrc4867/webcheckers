0bd7de70d6d765306f8e825ba9c2bed7279e3327
art4829 <abhaya.tmkr@gmail.com>
2019-03-27 10:57:51 -0400

    BoardView Tested

--- src/test/java/com/webcheckers/ui/BoardViewTest.java
+++ src/test/java/com/webcheckers/ui/BoardViewTest.java
@@ -6,8 +6,10 @@ import com.webcheckers.model.Board;
 import com.webcheckers.model.Color;
 import com.webcheckers.appl.Player;
 import com.webcheckers.ui.view.BoardView;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import com.webcheckers.ui.view.BoardView.Row;
 
 /**
  * Testing class for BoardView
@@ -17,14 +19,32 @@ import org.junit.jupiter.api.Test;
 @Tag("UI-tier")
 public class BoardViewTest {
 
-    private static Player r = new Player("r", Color.RED);
-    private static Player w = new Player("w", Color.WHITE);
-    private static Board b = new Board(r,w);
+    private static Player r;
+    private static Player w;
+    private static Board b;
+    private BoardView.Row row2;
+    private BoardView.Row row3;
 
+
+
+    @BeforeEach
+    public void setup(){
+        r = new Player("r", Color.RED);
+        w  = new Player("w", Color.WHITE);
+        b = new Board(r,w);
+        row2 = new BoardView(b,true).new Row(2,b,true);
+        row3 = new BoardView(b,true).new Row(2,b,true);
+
+
+    }
     @Test
     public void testCreation(){
         BoardView x = new BoardView(b, false);
         BoardView y = new BoardView(b, true);
+        assertThrows(IllegalArgumentException.class, () -> {
+            BoardView.Row row= new BoardView(b,true).new Row(-1,b,true);},"Invalid index");
+
+
         assertNotNull(x);
         assertNotNull(y);
     }
@@ -33,6 +53,17 @@ public class BoardViewTest {
     public void testIterator(){
         BoardView x = new BoardView(b, false);
         assertNotNull(x.iterator());
+        assertNotNull(row2.iterator());
     }
+
+
+    @Test
+    public void overridingTests(){
+        assertEquals(row2.getIndex(),2);
+        assertEquals(row2.hashCode(),2);
+        assertTrue(row2.equals(row3));
+
+    }
+
 
 }

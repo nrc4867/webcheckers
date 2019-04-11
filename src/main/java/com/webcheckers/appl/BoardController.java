f71f2638f38c14e07b958db0859a1cefbc46885f
Michael-Bianconi <michael.a.bianconi@gmail.com>
2019-04-11 11:11:38 -0400

    BoardController.getGameState()

--- src/main/java/com/webcheckers/appl/BoardController.java
+++ src/main/java/com/webcheckers/appl/BoardController.java
@@ -18,11 +18,12 @@ public class BoardController {
 
     /**
      * Move pieces on the board
+     *
      * @param moves a set of moves in order that they should be made, the moves have already been tested as valid
      */
     public void movePieces(ArrayList<Move> moves) {
-        for (Move move: moves) {
-            if(move.getMovement() == Move.MoveType.REGULAR) {
+        for (Move move : moves) {
+            if (move.getMovement() == Move.MoveType.REGULAR) {
                 makeMove(move);
             } else {
                 makeJump(move);
@@ -32,12 +33,13 @@ public class BoardController {
 
     /**
      * Move a piece on the board, the move has previously been tested as valid
+     *
      * @param move the move to make
      * @return the move was completed
      */
     public boolean makeMove(Move move) {
         Piece moved = board.getPiece(move.getStart());
-        if(moved == null) return false;
+        if (moved == null) return false;
         board.setPiece(null, move.getStart());
 
         moved.setCol(move.getEndCell());
@@ -51,11 +53,12 @@ public class BoardController {
     /**
      * make a jump move by removing a piece on the board while moving,
      * the move has previously been tested as valid
+     *
      * @param jump the jump to make
      * @return the jump was completed
      */
     public boolean makeJump(Move jump) {
-        if(!makeMove(jump)) return false;
+        if (!makeMove(jump)) return false;
         Piece middle = board.getPiece(jump.getJumpedPosition());
         board.setPiece(null, middle.getPos());
         return true;
@@ -63,6 +66,7 @@ public class BoardController {
 
     /**
      * Kings the Piece, if it can be kinged.
+     *
      * @param p Piece to king.
      * @return True if kinged.
      */
@@ -77,20 +81,21 @@ public class BoardController {
 
     /**
      * test to see if a move can be made, if the move is valid its movement type is set
-     * @param move the move to check
+     *
+     * @param move  the move to check
      * @param moves a list of valid moves already slotted to be preformed this turn
      * @return true if the move is valid in the context of moves
      */
     public boolean testMovement(Move move, ArrayList<Move> moves) {
         boolean testMove = false;
-        if(!mustJumpThisTurn(moves)) {
+        if (!mustJumpThisTurn(moves)) {
             testMove = canMoveTo(move, moves);
         }
 
         boolean testJump = canJumpTo(move, moves);
 
         if (testMove || testJump) {
-            move.setMovement((testMove)? Move.MoveType.REGULAR: Move.MoveType.JUMP);
+            move.setMovement((testMove) ? Move.MoveType.REGULAR : Move.MoveType.JUMP);
             return true;
         }
         return false;
@@ -98,6 +103,7 @@ public class BoardController {
 
     /**
      * check to see if there is a valid jump for this turn
+     *
      * @param moves the moves made this turn
      * @return true if there is a valid jump
      */
@@ -105,15 +111,15 @@ public class BoardController {
         Piece piece = board.getPiece(moves);
         Player activePlayer = board.getActivePlayer();
 
-        if(piece != null) {
+        if (piece != null) {
             return mustJumpThisTurn(piece, moves);
         }
 
-        for (Space[] rowspace: board.getSpaces()) {
-            for (Space space: rowspace) {
+        for (Space[] rowspace : board.getSpaces()) {
+            for (Space space : rowspace) {
                 piece = space.getPiece();
-                if(piece != null && activePlayer.ownsPiece(piece))
-                    if(mustJumpThisTurn(piece, moves)) return true;
+                if (piece != null && activePlayer.ownsPiece(piece))
+                    if (mustJumpThisTurn(piece, moves)) return true;
             }
         }
         return false;
@@ -121,6 +127,7 @@ public class BoardController {
 
     /**
      * Checks to see if a player must jump this turn
+     *
      * @param piece the piece multi-jumping
      * @param moves the moves already made
      * @return true if there are available multi-jumps
@@ -130,8 +137,8 @@ public class BoardController {
         final Position current = piece.getCurrentPosition(moves);
 
         Set<Move> possibleMoves = Move.generateMoves(current, 2);
-        for (Move move: possibleMoves) {
-            if(canJumpTo(move, moves)) {
+        for (Move move : possibleMoves) {
+            if (canJumpTo(move, moves)) {
                 return true;
             }
         }
@@ -140,12 +147,13 @@ public class BoardController {
 
     /**
      * checks to see if a move is valid in the context of other moves performed this turn
-     * @param move the move to check
+     *
+     * @param move  the move to check
      * @param moves a list of valid moves already slotted to be preformed this turn
      * @return true if the move is valid in the context of moves
      */
     public boolean canMoveTo(Move move, ArrayList<Move> moves) {
-        if(moves.size() != 0) return false; // You cannot make more than one regular move in a round
+        if (moves.size() != 0) return false; // You cannot make more than one regular move in a round
 
         Piece piece = board.getPiece(move, moves);
 
@@ -179,17 +187,18 @@ public class BoardController {
 
     /**
      * checks to see if a jump-move is valid in the context of other moves performed this turn
-     * @param move the move to check
+     *
+     * @param move  the move to check
      * @param moves a list of valid moves already slotted to be preformed this turn
      * @return true if the move is valid in the context of moves
      */
     public boolean canJumpTo(Move move, ArrayList<Move> moves) {
 
         // Check to make sure that the move connects with the previous moves
-        if(!moves.isEmpty() && !Move.ConnectedMoves(moves.get(moves.size() - 1), move)) return false;
+        if (!moves.isEmpty() && !Move.ConnectedMoves(moves.get(moves.size() - 1), move)) return false;
 
         // Cannot jump if the previous move was a not a jump
-        if(!moves.isEmpty() && moves.get(moves.size() -1).getMovement() == Move.MoveType.REGULAR) return false;
+        if (!moves.isEmpty() && moves.get(moves.size() - 1).getMovement() == Move.MoveType.REGULAR) return false;
 
         // Must jump diagonally 2 spaces
         if (!move.deltaRadius(2)) {
@@ -212,12 +221,12 @@ public class BoardController {
         // jumped-from position. The previous check will prevent
         // backtracking
         if (board.hasPiece(move.getEnd())
-        &&  !move.reenters(moves)) {
+                && !move.reenters(moves)) {
             return false;
         }
 
         Piece piece = board.getPiece(move, moves);
-        if(piece == null) {
+        if (piece == null) {
             return false; // the original piece should exist on the board
         }
 
@@ -240,7 +249,8 @@ public class BoardController {
 
     /**
      * Check to make sure a piece can move in a direction
-     * @param move the movement
+     *
+     * @param move  the movement
      * @param piece the piece
      * @return the move is valid for the piece
      */
@@ -249,13 +259,13 @@ public class BoardController {
         Position current = piece.getCurrentPosition(moves);
 
         if (piece.getColor() == Color.WHITE && !piece.isKing()
-                &&  move.getEndRow() < current.getRow()) {
+                && move.getEndRow() < current.getRow()) {
             return false;
         }
 
         // The Piece can't jump south if it's red and single
         if (piece.getColor() == Color.RED && !piece.isKing()
-                &&  move.getEndRow() > current.getRow()) {
+                && move.getEndRow() > current.getRow()) {
             return false;
         }
 
@@ -264,6 +274,7 @@ public class BoardController {
 
     /**
      * See if a piece should be kinged based on a move
+     *
      * @param move the move to check
      * @return true if a piece is kinged
      */
@@ -275,6 +286,7 @@ public class BoardController {
 
     /**
      * Check if a piece should be kinged based on a series of moves
+     *
      * @param moves the moves to make
      * @return true if the piece is kinged
      */
@@ -283,9 +295,9 @@ public class BoardController {
 
         Move lastMove = moves.get(moves.size() - 1);
 
-        if(piece.isKing()) return false;
-        if(piece.getColor() == Color.WHITE && lastMove.getEndRow() == Board.getSize() - 1) return true;
-        if(piece.getColor() == Color.RED && lastMove.getEndRow() == 0) return true;
+        if (piece.isKing()) return false;
+        if (piece.getColor() == Color.WHITE && lastMove.getEndRow() == Board.getSize() - 1) return true;
+        if (piece.getColor() == Color.RED && lastMove.getEndRow() == 0) return true;
         return false;
     }
 
@@ -315,6 +327,7 @@ public class BoardController {
 
     /**
      * Check the active player
+     *
      * @param player the player to check
      * @return true if player is active
      */
@@ -325,6 +338,7 @@ public class BoardController {
 
     /**
      * Remove a player from play
+     *
      * @param player the player to resign
      */
     public void resign(Player player) {
@@ -340,4 +354,45 @@ public class BoardController {
     }
 
 
+    /**
+     * Checks whether the active player has any moves remaining.
+     * If the active player has no moves, then they have lost.
+     * 
+     * Only call at the beginning of the turn!
+     *
+     * @return Returns INPROGRESS, RED_WON, WHITE_WON
+     */
+    public GameState getGameState() {
+
+        // Iterate through every space on the board
+        for (Space[] rowspace : board.getSpaces()) {
+            for (Space space : rowspace) {
+                Piece piece = space.getPiece();
+
+                // Space does not contain a piece
+                if (piece == null) continue;
+
+                // Piece not owned by active player, ignore
+                if (!board.getActivePlayer().ownsPiece(piece)) continue;
+
+                // Piece can jump
+                if (mustJumpThisTurn(piece, new ArrayList<>()))
+                    return GameState.INPROGRESS;
+
+                // Piece can move
+                Set<Move> possible = Move.generateMoves(piece.getPos(), 1);
+                for (Move m : possible) {
+                    if (canMoveTo(m, new ArrayList<>())) {
+                        return GameState.INPROGRESS;
+                    }
+                }
+            }
+        }
+
+        // No possible moves!
+        if (board.getActivePlayer().equals(board.getRedPlayer()))
+            return GameState.WHITE_WON;
+
+        else return GameState.RED_WON;
+    }
 }
